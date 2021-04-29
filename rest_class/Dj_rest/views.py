e98a4df008c7682451a83985bd2e8f0fe4dc8d15
Winner Abiodun <80297978+winnerabiodun1@users.noreply.github.com>
2021-04-29 03:54:47 +0100

    Progress///down to the login phase///

--- rest_class/Dj_rest/views.py
+++ rest_class/Dj_rest/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+def home(request):
+    data = {
+        "message" :"This is a json response"
+    }
+    return JsonResponse(data)
--- rest_class/rest_class/settings.py
+++ rest_class/rest_class/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Dj_rest',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
